6cabb7dfa47c731c06de11380cad49585fd6c887
yhhu <yhhu2049@gmail.com>
2018-03-30 22:15:11 +0800

    test

---
+++ temp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-3-30 下午10:06
+# @Author  : Hu Yuhang
+# @Site    : 
+# @File    : temp.py
+# @Software: PyCharm
+def defa():
+    a = 1
+    b = 2
+    c = a+b
+    return c
